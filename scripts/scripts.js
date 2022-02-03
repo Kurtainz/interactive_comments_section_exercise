4749bd0efd52c4ac3ba5ad7522febbd07ac97f6b
Kurtainz <kurtainz@googlemail.com>
2022-02-03 22:56:36 +0000

    Comment styles are almost complete

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -24,6 +24,7 @@ const addCommentToScreen = (commentObj, isReply) => {
     commentTemplate.querySelector('.profileName h2').innerText = commentObj.user.username;
     commentTemplate.querySelector('.postRecency p').innerText = createDateDescription(new Date(commentObj.createdAt));
     commentTemplate.querySelector('.commentText').innerText = commentObj.content;
+    commentTemplate.querySelector('.rating').innerText = commentObj.score;
 
     // Add to screen
     const newCommentBox = document.getElementById('addComment');
@@ -87,50 +88,3 @@ const setComments = () => {
         }
     });
 }
-
-// const init = () => {
-//     const getData = async () => {
-//         const response = await fetch('scripts/data.json');
-
-//         if (!response.ok) {
-//             console.log(response.status);
-//             return null;
-//         }
-
-//         return response.json();
-//     }
-
-//     const addCommentToScreen = (commentObj, isReply) => {
-//         // Need to ensure ID is changed
-//         const commentTemplate = document.getElementById('commentTemplate').content.cloneNode(true);
-
-//         // Set profile images
-//         commentTemplate.querySelector('.profileImage picture source').setAttribute('srcset', commentObj.user.image.webp);
-//         commentTemplate.querySelector('.profileImage picture img').setAttribute('src', commentObj.user.image.png);
-
-//         commentTemplate.querySelector('.profileName h2').innerText = commentObj.user.username;
-//         commentTemplate.querySelector('.postRecency p').innerText = commentObj.user.username;
-//         commentTemplate.querySelector('.postRecency p').innerText = commentObj.user.username;
-
-//     }
-
-//     const createDateDescription = date => {
-
-//     }
-
-//     getData().then(response => {
-//         localStorage.setItem('currentUserData', JSON.stringify(response.currentUser));
-//         localStorage.setItem('commentData', JSON.stringify(response.comments));
-//     });
-
-//     // TODO add profile image for new comment box
-
-//     // TODO append to screen using template
-//     const comments = JSON.parse(window.localStorage.getItem('commentData'));
-
-//     comments.forEach(parentCommentObj => {
-
-//     });
-// }
-
-// window.onload = init();
